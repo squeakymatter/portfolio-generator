fc33ede53e4403bcb450dcb290ae979a848d1453
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2021-02-01 02:54:12 -0800

    forgot app.js

--- app.js
+++ app.js
@@ -1,30 +1,54 @@
-//when you run this app: 
-// node app.js 'Michelle' 'Web Developer', it will return an array of name and occupation. 
-// This is because you used the array method .slice() to return a brand-new array based on `process.argv starting at the third index (i.e., index 2 in the zero-based array) and ending with the final index.
-//slice() returns everything from teh first zero-based index we provide as the first argument up to but not including the zero-based index we provide in the second argument. So, to return through the last index in the array, we provde the length of the array as the second argument. This way, you don't actually maniuplated process.argv, but rather create a new array based on teh values from the third index on.
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
 
-//Notice the lack of parenthesis around the `profileDataArr` parameter. Since we only have one parameter, we can omit the parenthesis wrapping it.
-const printProfileData = profileDataArr => {
-	//This...
-	for (let i = 0; i < profileDataArr.length; i++) {
-		console.log(profileDataArr[i]);
-	}
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+//Or, written with oneline:
+const [name, github] = profileDataArgs;
 
-	console.log('================');
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
 
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-	});
+// //   console.log('================');
 
-	console.log('================');
+// //   // Is the same as this...
+// //   profileDataArr.forEach(profileItem => console.log(profileItem));
+// // };
 
-	// is the same as this...
-	profileDataArr.forEach(profileItem => console.log(profileItem));
+// // printProfileData(profileDataArgs);
+
+// // const generatePage = () => 'Name: Jane, Github: janehub';
+// //This returns a string.
+// //parenthesis are unnecessary in arrow functions when there is one parameter, but if function has no parameters, use parenthesis to hold the place of parameters.
+// //note there is no `return` keyword. This is a special case where the function only has a single statement, so the curly braces are unnecessary and the return statement is implied.
+// //This should return a string:
+// // console.log(generatePage());
+
+// ///////////////
+// //Template Literals
+// // const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+
+// // console.log(generatePage('Jane', 'janehub'));
+
+// //Multi-line Strings use backticks...
+// const generatePage = (userName, githubName) => {
+//   return `
+//     Name: ${userName}
+//     GitHub: ${githubName}
+//   `;
+// };
+// console.log(generatePage('Jane', 'janehub'));
 
 
-};
+// console.log(name,github);
+// console.log(generatePage(name, github));
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+	if (err) throw err;
+
+	console.log('Portfolio complete! Check out index.html to see the output.');
+});
