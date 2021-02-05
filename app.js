71ffc685d74e174bc43d4b3d8a6268a540ae3939
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2021-02-05 00:32:31 -0800

    cleanup

--- app.js
+++ app.js
@@ -1,70 +1,18 @@
-<<<<<<< HEAD
 const fs = require('fs');
-const generatePage = require('./src/page-template.js'); //gets the generatePage variable from page-template.js
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
 
 const [name, github] = profileDataArgs;
 
+console.log(name, github);
 
+const pageHTML = generatePage(name, github);
 
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
   console.log('Portfolio complete! Check out index.html to see the output!');
-=======
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-//Or, written with oneline:
-const [name, github] = profileDataArgs;
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-// //   console.log('================');
-
-// //   // Is the same as this...
-// //   profileDataArr.forEach(profileItem => console.log(profileItem));
-// // };
-
-// // printProfileData(profileDataArgs);
-
-// // const generatePage = () => 'Name: Jane, Github: janehub';
-// //This returns a string.
-// //parenthesis are unnecessary in arrow functions when there is one parameter, but if function has no parameters, use parenthesis to hold the place of parameters.
-// //note there is no `return` keyword. This is a special case where the function only has a single statement, so the curly braces are unnecessary and the return statement is implied.
-// //This should return a string:
-// // console.log(generatePage());
-
-// ///////////////
-// //Template Literals
-// // const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-
-// // console.log(generatePage('Jane', 'janehub'));
-
-// //Multi-line Strings use backticks...
-// const generatePage = (userName, githubName) => {
-//   return `
-//     Name: ${userName}
-//     GitHub: ${githubName}
-//   `;
-// };
-// console.log(generatePage('Jane', 'janehub'));
-
-
-// console.log(name,github);
-// console.log(generatePage(name, github));
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-	if (err) throw err;
-
-	console.log('Portfolio complete! Check out index.html to see the output.');
->>>>>>> fc33ede53e4403bcb450dcb290ae979a848d1453
 });
--- src/page-template.js
+++ src/page-template.js
@@ -1,34 +1,19 @@
-const generatePage = (name, github) => {
-<<<<<<< HEAD
+module.exports = (name, github) => {
   return `
-  <!DOCTYPE html> 
-=======
-	return `
-	<!DOCTYPE html> 
->>>>>>> fc33ede53e4403bcb450dcb290ae979a848d1453
-  <html lang="en"> 
+  <!DOCTYPE html>
+  <html lang="en">
+  
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portfolio Demo</title>
   </head>
-
+  
   <body>
     <h1>${name}</h1>
     <h2><a href="https://github.com/${github}">Github</a></h2>
   </body>
   </html>
-<<<<<<< HEAD
   `;
 };
-
-	module.exports = generatePage; 
-	// now go to app.js file and add const generatePage = ('./src/page-template.js');
-=======
-	`;
-};
-
-//in orderto use functions from one module inside another, we use related statements `module.exports` and `require`. In source file that has functions, use `module.exports` at the bottom. In the destinate ifles that we want to receive these functions, we put `require` at the top.
-module.exports = generatePage;
->>>>>>> fc33ede53e4403bcb450dcb290ae979a848d1453
